edcbbeed649976dd3462c8004092b75b2911c062
Matteo Ferrando <matteo.ferrando2@gmail.com>
2017-07-26 18:20:10 -0400

    added SSL to record declaration

--- include/pgsql_listen.hrl
+++ include/pgsql_listen.hrl
@@ -21,7 +21,7 @@
 -define(DEFAULT_SSL_OPTS, <<"{}">>).
 
 -record(pgsql_listen_conn, {pid, server, dbname}).
--record(pgsql_listen_dsn, {host, port, user, password, dbname}).
+-record(pgsql_listen_dsn, {host, port, user, password, dbname, ssl, ssl_opts}).
 -record(pgsql_listen_state, {amqp, channels, pgsql}).
 -record(properties, {app_id, content_type, content_encoding, delivery_mode,
                      headers, priority, reply_to, type}).
